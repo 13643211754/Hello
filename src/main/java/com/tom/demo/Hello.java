d29adf10161a9ca5f8ac5aac90480b5a2b7914be
cfx <505027363@qq.com>
2021-03-01 09:46:42 +0800

    这是一个注释~~~

--- src/main/java/com/tom/demo/Hello.java
+++ src/main/java/com/tom/demo/Hello.java
@@ -1,7 +1,11 @@
 package com.tom.demo;
 
+/**
+ * 注释你好
+ */
 public class Hello {
     public static void main(String[] args) {
         System.out.println("hello");
+        System.out.println("注释!");
     }
 }
