63c9e92f0998a1bea95f2ed290900d21fcf36d6a
cfx <505027363@qq.com>
2021-03-01 11:07:40 +0800

    处处

--- src/main/java/com/tom/demo/Hello.java
+++ src/main/java/com/tom/demo/Hello.java
@@ -6,10 +6,7 @@ package com.tom.demo;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("hello");
-        System.out.println("注释!");
-        System.out.println("第三次");
-        System.out.println("修复bug");
-        System.out.println("完成注册");
-        
+
+
     }
 }
